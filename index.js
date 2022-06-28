de46878e50ed33f6d83641087c6aae2cbc8a8114
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-28 17:57:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Cat {
+    //...
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+    speak() {
+        console.log(this.name + " says meow!");
+    };
+  }
+  class Dog {
+    //...
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        this.speak =function(){
+            console.log(this.name + " says woof!");
+        };
+    };
+  }
+//     };
+//     speak() {
+//         console.log(this.name + " says woof!");
+//     };
+//   }
+  
+  class Bird {
+    //...
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+    speak() {
+        console.log("It's me! , the parrot!");
+    };
+  }
